ef3219131171abdd3235c7e4d4950a6d5a621c2d
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-09 21:51:09 +0300

    Task completed

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        int intC = (int) c;
+        int intH = (int) h;
+        int intA = (int) a;
+        int intR = (int) r;
+
+        System.out.println(intC);
+        System.out.println(intH);
+        System.out.println(intA);
+        System.out.println(intR);
     }
 }
